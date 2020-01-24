4fa6352df099dfe6a7b6588da4ae6d28d194afe0
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-24 20:35:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	puts programmer_hash[:dennis_ritchie][:languages]
+	puts programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
